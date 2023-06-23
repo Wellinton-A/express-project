25a422be2e627fadfda1c90a921b13c29d45c21c
Wellinton Alexandre <wellinton1067@gmail.com>
2023-06-23 16:00:13 -0300

    added sendFile

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,5 +1,9 @@
+const process = require('process')
+const path = require('path')
+
 function getMessages(req ,res) {
-  res.send('<ul><li>Hello George!</li></ul>')
+  // res.send('<ul><li>Hello George!</li></ul>')
+  res.sendFile(path.join(process.cwd(), 'public', 'mountains.jpg'))
 }
 
 module.exports = {
